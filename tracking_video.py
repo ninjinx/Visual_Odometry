a784b850ac9c7506395739f7ec2bef00928aae4e
ninjinx <thomastolbol@gmail.com>
2017-12-04 14:21:57 +0100

    Added experimental scale estimation and features are now retained between keyframes

--- tracking_video.py
+++ tracking_video.py
@@ -37,7 +37,43 @@ def draw_tracks(canvas, points1, points2):
         canvas = cv2.line(canvas, (a, b), (c, d), (0, 255, 0), 1)
     return canvas
 
-min_features = 80
+
+def get_mean_distance_2d(features1, features2):
+    num_features = min((len(features1), len(features2)))
+    features1 = np.reshape(features1, (num_features, 2))
+    features2 = np.reshape(features2, (num_features, 2))
+
+    features = zip(features1, features2)
+    n = 0
+    dist = 0
+    for f1, f2 in features:
+        dx = f1[0]-f2[0]
+        dy = f1[1]-f2[1]
+        d = math.sqrt(dx**2+dy**2)
+        dist += d
+        n += 1
+
+    if n == 0:
+        return 0
+
+    dist /= n
+    return dist
+
+
+def update_motion(points1, points2, Rp, Tp, scale=1.0):
+    p1 = np.reshape(np.array(points1, dtype=np.float32), (-1, 1, 2))
+    p2 = np.reshape(np.array(points2, dtype=np.float32), (-1, 1, 2))
+    E, mask = cv2.findEssentialMat(p1, p2,
+                                   cameraMatrix=cam_mat, method=cv2.RANSAC, prob=0.999, threshold=1.0, mask=None)
+    points, R, T, newmask = cv2.recoverPose(E, p1, p2, cameraMatrix=cam_mat, mask=mask)
+
+    Tp = Tp + np.dot(R, T)*scale
+    Rp = np.dot(R, Rp)
+    return Rp, Tp
+
+
+min_features = 300
+max_dist = 80
 
 fps = FPS().start()
 
@@ -63,7 +99,7 @@ lk_params = dict(winSize=(15, 15),
 # params for ShiTomasi corner detection
 feature_params = dict(maxCorners=8000,
                       qualityLevel=0.01,
-                      minDistance=10,
+                      minDistance=5,
                       blockSize=3,
                       useHarrisDetector=False,
                       k=0.04)
@@ -75,19 +111,14 @@ cap = cv2.VideoCapture('VID_20171120_134746.mp4')
 prev_frame = None
 prev_gray = None
 
-keyframe_old = None
-keyframe_new = None
 keypoint_dist = 0
-frames = 0
-keyframes = 0
 
-detector = cv2.BRISK_create()
-norm = cv2.NORM_HAMMING
-matcher = cv2.BFMatcher(norm)
+old_points = []
+new_points = []
+
+keypoints = []
 
 while cap.isOpened():
-    frames += 1
-
     ret, frame = cap.read()
     if not ret:
         break
@@ -95,82 +126,98 @@ while cap.isOpened():
     if prev_frame is None:
         prev_frame = cv2.resize(frame, dsize=(w, h))
         prev_gray = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
-        keyframe_new = np.copy(prev_gray)
         continue
 
     frame = cv2.resize(frame, dsize=(w, h))
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    points1 = cv2.goodFeaturesToTrack(prev_gray, mask=None, **feature_params)
-    points2, status, error = cv2.calcOpticalFlowPyrLK(prev_gray, gray, points1, None, **lk_params)
-    good_old = points1[status == 1]
-    good_new = points2[status == 1]
+    if keypoint_dist > max_dist:
+        # update motion
+        if len(keypoints) > 7 and len(new_points) > 7:
+            old_pos = np.copy(Tpos) # copy old position for drawing of path
+            Rpos, Tpos = update_motion(new_points, keypoints, Rpos, Tpos, scale=keypoint_dist)
 
-    # calculate mean distance
-    mean_dist = 0
-    n = 0
-    for i, p2 in enumerate(good_new):
-        p1 = good_old[i]
-
-        if p1 is None or p2 is None:
-            continue
-
-        if hasattr(p1, '__len__') is False:
-            continue
-
-        if hasattr(p2, '__len__') is False:
-            continue
-
-        if len(p1) <= 1 or len(p2) <= 1:
-            continue
-
-        dx = float(p1[0]) - float(p2[0])
-        dy = float(p1[1]) - float(p2[1])
-        mean_dist += math.sqrt(dx ** 2 + dy ** 2)
-        n += 1
-
-    if n > 0:
-        mean_dist /= n
-        keypoint_dist += mean_dist
-
-    if keypoint_dist > 20:  # TODO change to variable
-        keyframes += 1
-        keypoint_dist = 0
-        keyframe_old = np.copy(keyframe_new)
-        keyframe_new = np.copy(gray)
-
-        points1 = cv2.goodFeaturesToTrack(keyframe_old, mask=None, **feature_params)
-        points2, status, error = cv2.calcOpticalFlowPyrLK(keyframe_old, keyframe_new, points1, None, **lk_params)
-        good_old = points1[status == 1]
-        good_new = points2[status == 1]
-
-        fade = np.ones((h, w, 3), dtype=np.uint8)*30
-        canvas = np.zeros((h, w, 3), dtype=np.uint8)
-        canvas = draw_tracks(canvas, good_old, good_new)
-        print(len(good_new))
-
-        if len(good_new) > 7:
-            good_new = np.reshape(np.array(good_new, dtype=np.float32), (-1, 1, 2))
-            good_old = np.reshape(np.array(good_old, dtype=np.float32), (-1, 1, 2))
-            E, mask = cv2.findEssentialMat(good_new, good_old,
-                                           cameraMatrix=cam_mat, method=cv2.RANSAC, prob=0.999, threshold=1.0, mask=None)
-            points, R, T, newmask = cv2.recoverPose(E, good_new, good_old, cameraMatrix=cam_mat, mask=mask)
-
-            old_pos = np.array(Tpos)
-            Tpos = Tpos + np.dot(Rpos, T)
-            Rpos = np.dot(R, Rpos)
-
-            x1 = int(old_pos[0, 0] * 5 + w / 2)
-            y1 = int(old_pos[1, 0] * 5 + h / 2)
-            x2 = int(Tpos[0, 0] * 5 + w / 2)
-            y2 = int(Tpos[1, 0] * 5 + h / 2)
+            #draw path
+            x1 = int(old_pos[0, 0] * (3/32) + w / 2)
+            y1 = int(old_pos[1, 0] * (3/32) + h / 2)
+            x2 = int(Tpos[0, 0] * (3/32) + w / 2)
+            y2 = int(Tpos[1, 0] * (3/32) + h / 2)
             cv2.line(path, (x1, y1), (x2, y2), (0, 255, 0), 1)
 
-            # show result
-            frame = cv2.add(frame, canvas)
-            cv2.imshow("Canvas", canvas)
-            cv2.imshow("path", path)
-            cv2.imshow("Frame", frame)
+        # select new keypoints
+        keypoint_dist = 0
+        old_points = cv2.goodFeaturesToTrack(prev_gray, mask=None, **feature_params)
+        keypoints = np.copy(old_points)
+    elif len(new_points) < min_features:
+        # update motion
+        if len(keypoints) > 7 and len(new_points) > 7:
+            old_pos = np.copy(Tpos)  # copy old position for drawing of path
+            Rpos, Tpos = update_motion(new_points, keypoints, Rpos, Tpos, scale=keypoint_dist)
+
+            # draw path
+            x1 = int(old_pos[0, 0] * (3/32) + w / 2)
+            y1 = int(old_pos[1, 0] * (3/32) + h / 2)
+            x2 = int(Tpos[0, 0] * (3/32) + w / 2)
+            y2 = int(Tpos[1, 0] * (3/32) + h / 2)
+            cv2.line(path, (x1, y1), (x2, y2), (0, 255, 0), 1)
+
+        # select new keypoints
+        keypoint_dist = 0
+        old_points = cv2.goodFeaturesToTrack(prev_gray, mask=None, **feature_params)
+        keypoints = np.copy(old_points)
+    else:
+        # check number of features in each quadrant to ensure a good distribution of features across entire image
+        nw = 0
+        ne = 0
+        sw = 0
+        se = 0
+        for x, y in new_points:
+            if x > w//2:
+                if y > h//2:
+                    se += 1
+                else:
+                    ne += 1
+            else:
+                if y > h//2:
+                    sw += 1
+                else:
+                    nw += 1
+
+        num_features = min((nw, ne, sw, se))
+        if num_features < min_features//4:
+            # update motion
+            if len(keypoints) > 7 and len(new_points) > 7:
+                old_pos = np.copy(Tpos)  # copy old position for drawing of path
+                Rpos, Tpos = update_motion(new_points, keypoints, Rpos, Tpos, scale=keypoint_dist)
+
+                # draw path
+                x1 = int(old_pos[0, 0] * (3/32) + w / 2)
+                y1 = int(old_pos[1, 0] * (3/32) + h / 2)
+                x2 = int(Tpos[0, 0] * (3/32) + w / 2)
+                y2 = int(Tpos[1, 0] * (3/32) + h / 2)
+                cv2.line(path, (x1, y1), (x2, y2), (0, 255, 0), 1)
+
+            # select new keypoints
+            keypoint_dist = 0
+            old_points = cv2.goodFeaturesToTrack(prev_gray, mask=None, **feature_params)
+            keypoints = np.copy(old_points)
+        else:
+            dim = np.shape(new_points)
+            old_points = np.reshape(new_points, (-1, 1, 2))
+
+    new_points, status, error = cv2.calcOpticalFlowPyrLK(prev_gray, gray, old_points, None, **lk_params)
+    keypoints = np.reshape(keypoints, (-1, 1, 2)) # TODO find out why this is necessary?!
+    old_points = old_points[status == 1]
+    new_points = new_points[status == 1]
+    keypoints = keypoints[status == 1]
+
+    keypoint_dist += get_mean_distance_2d(old_points, new_points)
+
+    frame = draw_tracks(frame, old_points, new_points)
+    cv2.line(frame, (0, h // 2), (w, h // 2), (255, 0, 0), 1)
+    cv2.line(frame, (w // 2, 0), (w // 2, h), (255, 0, 0), 1)
+    cv2.imshow("Frame", frame)
+    cv2.imshow("Path", path)
 
     prev_gray = gray.copy()
     key = cv2.waitKey(1) & 0xFF
@@ -195,8 +242,6 @@ while cap.isOpened():
 fps.stop()
 print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
 print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-print(frames)
-print(keyframes)
 # do a bit of cleanup
 cv2.destroyAllWindows()
 cap.release()
